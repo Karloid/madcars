a94f08a0b3ba947ef6ee809fd9fa25a72161f1b1
dmitry <my@my.ru>
2018-09-05 00:21:59 +0300

    - work

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import json
 import random
 import sys
 
+
 def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+    pass
+    # print(*args, file=sys.stderr, **kwargs)
+
 
 while True:
     z = input()
     eprint(z)
-    commands = ['left', 'right', 'stop']
+    commands = ['left', 'right']
     cmd = random.choice(commands)
     print(json.dumps({"command": cmd, 'debug': cmd}))
